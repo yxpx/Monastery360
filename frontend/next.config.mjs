56fa789a03840ae7b2f91b1c6d4308560c7127c3
Anirudh Navuduri <nanirudh1107@gmail.com>
2025-09-10 00:11:16 +0530

    pwa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
+
+import pkg from 'next-pwa';
+const withPWA = pkg.default || pkg;
+
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +19,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default withPWA(pwaConfig)(nextConfig);
